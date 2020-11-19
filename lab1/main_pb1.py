60d5ddce8e40aedc9d904c384e3020afb1db4c69
Lensca84 <jules.callens84@gmail.com>
2020-11-19 10:40:15 +0100

    question 1b

--- lab1/main_pb1.py
+++ lab1/main_pb1.py
@@ -15,18 +15,39 @@ maze = np.array([
 
 # Create an environment maze
 env = mz.Maze(maze)
-env.show()
+#env.show()
 
-# Finite horizon
-horizon = 20
-# Solve the MDP problem with dynamic programming
-V, policy= mz.dynamic_programming(env,horizon);
+# # Finite horizon
+# horizon = 20
+# # Solve the MDP problem with dynamic programming
+# V, policy= mz.dynamic_programming(env,horizon);
+#
+# # Simulate the shortest path starting from position A
+# method = 'DynProg';
+# start  = (0,0);
+# start_min = (6,5)
+# path = env.simulate(start, start_min, policy, method);
 
-# Simulate the shortest path starting from position A
+# Show the shortest path
+#mz.animate_solution(maze, path)
+
+N = 1000
 method = 'DynProg';
 start  = (0,0);
 start_min = (6,5)
-path = env.simulate(start, start_min, policy, method);
+tab = np.zeros(16)
 
-# Show the shortest path
-mz.animate_solution(maze, path)
+for T in range(14,30):
+    count = 0
+    print('Horizon = ', T)
+    V, policy= mz.dynamic_programming(env,T)
+    for _ in range(N):
+        path = env.simulate(start, start_min, policy, method);
+        if path[-1].player_pos == start_min:
+            count += 1
+    tab[T-14] = count/N
+
+plt.figure(1)
+Time = np.arrange(14,30)
+plt.plot(Time,tab)
+plt.show()
